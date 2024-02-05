69308218128877a6bf51678f0b1e22e632becb9e
Victorschmidtnti <118182764+Victorschmidtnti@users.noreply.github.com>
2024-02-05 14:39:02 +0100

    v.6

--- app.rb
+++ app.rb
@@ -1,13 +1,51 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
 
 
-enable :seassions
+enable :sessions
+user-id=1
+user-id = session[:key1]
 
 
-get('/hemsida') do
-    slim(:hemsida)
+get('/')  do
+  slim(:start)
+end 
+
+get('/gymlog') do 
+    db = SQLite3::Database.new("db/user.db")  
+    db.results_as_hash = true
+    @result = db.execute("SELECT * FROM gymlog")
+    
+    slim(:"gymlog/index")
 end
+
+post('/gymlog/:id/delete') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/user.db")  
+    db.execute("DELETE FROM gymlog WHERE id = ?",id)
+    redirect('/gymlog')
+end
+
+get('/gymlog/new') do
+    slim(:"gymlog/new") 
+end
+
+post('/gymlog/new') do 
+    dag = params[:dag]
+    exercises = params[:exercises].to_i
+    db = SQLite3::Database.new("db/user.db") 
+    db.execute("INSERT INTO gymlog (dag, exercises) VALUES (?,?)" ,dag, exercises)
+    redirect('/gymlog')
+end
+
+
+
+
+
+
+
+
