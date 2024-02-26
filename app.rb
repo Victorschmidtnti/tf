210a77cb832eb1cfe98d5dc886ab97239ca07777
Victorschmidtnti <118182764+Victorschmidtnti@users.noreply.github.com>
2024-02-26 14:39:49 +0100

    v.9

--- app.rb
+++ app.rb
@@ -70,11 +70,48 @@ end
 get('/index2/:type_off') do
     db = SQLite3::Database.new("db/user.db")  
     db.results_as_hash = true
-    type_off=params[:type_off]
-    @result = db.execute("SELECT * FROM exercises WHERE 'type-id'=?",type_off)
-    
+    type_off=params[:type_off].to_i
+    @result = db.execute("SELECT * FROM exercises WHERE \"type-id\" = ?", type_off)
+    slim(:"exercises/index3")
 end
 
+post('/exercises/:id/delete') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/user.db")  
+    db.execute("DELETE FROM exercises WHERE id = ?",id)
+    redirect('/type')
+end
 
+# Route för att visa formuläret för att lägga till en ny övning
+get('/exercises/new') do
+    db = SQLite3::Database.new("db/user.db")  
+    db.results_as_hash = true
+    @result = db.execute("SELECT * FROM type") # Hämta alla muskelgrupper för dropdown-menyn
+    slim(:"exercises/new")
+  end
+  
+  # Route för att ta emot formulärdata och lägga till en ny övning
+  post('/exercises/new') do 
+    exercises = params[:exercises]
+    type_id = params[:type_id].to_i # Det valda värdet från dropdown-menyn
+    db = SQLite3::Database.new("db/user.db") 
+    db.execute("INSERT INTO exercises (exercises, \"type-id\") VALUES (?, ?)", exercises, type_id)
+    redirect('/type')
+  end
+  
 
+get('/exercises/:id/edit') do
+    id=params[:id].to_i
+    db = SQLite3::Database.new("db/user.db")
+    db.results_as_hash = true
+    @result = db.execute("SELECT * FROM exercises WHERE id=?",id).first
+    slim(:"exercises/edit")
+end
 
+post('/exercises/:id/update') do
+    id=params[:id].to_i
+    exercises=params[:exercises]
+    db = SQLite3::Database.new("db/user.db")
+    db.execute("UPDATE exercises SET exercises=? WHERE id = ?",exercises,id)
+    redirect('/type')
+end
